1fc476c4ed7ace3f3f5e74e74dd7be89f07e70d7
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-07-09 18:25:02 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CardComponent } from './card/card.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule
